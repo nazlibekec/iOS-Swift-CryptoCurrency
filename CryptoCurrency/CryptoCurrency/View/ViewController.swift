3adc0d5dcf476b026e7d590fdfdb767fb9a93b07
nazlibekec <nazlibekec@gmail.com>
2023-04-10 17:05:10 +0300

    Verileri gösterme işlemi tamamlandı.

--- CryptoCurrency/CryptoCurrency/View/ViewController.swift
+++ CryptoCurrency/CryptoCurrency/View/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    private var CryptoListViewModel : CryptoListViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,18 +19,43 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.delegate = self
         tableView.dataSource = self
         
+        getData()
+                
+    }
+    
+    func getData() {
         let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!
         webService().downloadCurrencies(url: url) { (cryptos) in
             if let cryptos = cryptos {
-              print(cryptos)
+              
+                // tüm cryptos lardan oluşan listeyi viewmodele verdik.
+                self.CryptoListViewModel = CryptoCurrency.CryptoListViewModel(cryptoCurrencyList: cryptos)
+                
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                
+                }
             }
         }
     }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        
+        // getData fonksiyonu daha önce çalışacağı için ve cryptoListViewModel ilk çalıştığında nil gelebilir ve uygulama çöker.
+        // Bu sebeple crptoListViewModel nil ise 0 değilse ViewModel de oluşturduğumuz numberOfRowsInSection fonksiyonunu çağır.
+        return self.CryptoListViewModel == nil ? 0 : self.CryptoListViewModel.numberOfRowsInSection()
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CryptoCell", for: indexPath) as! CryptoTableViewCell
+        
+        //her bir cell için yeni bir cryptoViewModel oluşturulacak.
+        let cryptoViewModel = self.CryptoListViewModel.cryptoAtIndex(indexPath.row)
+        
+        cell.priceText.text = cryptoViewModel.price
+        cell.currencyText.text = cryptoViewModel.name
+        
         return cell
     }
 }
